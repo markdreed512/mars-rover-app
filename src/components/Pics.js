b1ea7dd018abf47dc27299c96c8aff4252de9bd8
Mark Reed <markdreed512@gmail.com>
2020-07-20 11:33:02 -0500

    get manifest and set most recent date

--- src/components/Pics.js
+++ src/components/Pics.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const Img = styled.img`
@@ -9,23 +9,30 @@ const Div = styled.div`
 `
 function Pics() {
 
-    const [date, setDate] = useState("test")
+    const [mostRecentDate, setMostRecentDate] = useState("2019-1-1")
     const [picData, setPicData] = useState({})
-    const displayPics = () => {
-        console.log("hi")
-        fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2020-05-1&api_key=6cwPKDw2dwUyxJbFgml1JUuHYRfCegYLtZtkBVdH')
-            .then(response => {
-                return response.json()
-            })
+    const [rover, setRover] = useState("curiosity")
+   
+    const fetchManifestAndSetMostRecentDate = () => {
+        let url = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?&api_key=6cwPKDw2dwUyxJbFgml1JUuHYRfCegYLtZtkBVdH`
+        fetch(url)
+        .then(response => response.json())
+        .then(manifestData => 
+            setMostRecentDate(manifestData.photo_manifest.max_date)    
+        )
+    }
+    useEffect(()=>{
+        fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${mostRecentDate}&api_key=6cwPKDw2dwUyxJbFgml1JUuHYRfCegYLtZtkBVdH`)
+            .then(response => response.json())
             .then(data => {
                 setPicData(data)
                 console.group(data)
             })
-
-    }
+    },[mostRecentDate, rover])
     return (
         <div>
-            <button onClick={displayPics}>Most Recent Pics</button><br />
+            {/* <button onClick={handleCurio}>get manifest</button> */}
+            <button onClick={fetchManifestAndSetMostRecentDate}>Most Recent Pics</button><br />
             {/* <h2>Pics for {date}</h2> */}
             {picData.photos ?
                 picData.photos.map((item, i) => {
